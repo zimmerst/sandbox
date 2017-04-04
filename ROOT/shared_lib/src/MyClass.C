70552d16594f9332688a0c8190317e6a335dd081
Hugh Dickinson <hughdickinson@users.noreply.github.com>
2017-04-04 18:51:57 -0500

    Update MyClass.C

--- ROOT/shared_lib/src/MyClass.C
+++ ROOT/shared_lib/src/MyClass.C
@@ -2,7 +2,22 @@
 
 ClassImp(MyClass);
 
+void MyClass::internalMethod(){
+	std::cout << "calling an internal method" << std::endl;
+}
+
+public:
+
+MyClass::MyClass(TString *fname){
+	fFile = TFile::Open(*fname,"RECREATE");
+}
+
+MyClass::~MyClass(){
+	fFile->Write();
+	fFile->Close();
+	delete fFile;
+}
+
 void MyClass::run(){
 	this->internalMethod();
 }
-
